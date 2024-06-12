6035188c9054a137ba19ea010af55e9bb01de71a
C0d3crush <lukas.dzielski@gmail.com>
2024-06-13 00:42:27 +0200

    fix logic error

--- Board.cpp
+++ Board.cpp
@@ -38,12 +38,12 @@ int Board::get_index(int x, int y) {
 
 int Board::get_x(int index)
 {
-    return w / index;
+    return index / w;
 }
 
 int Board::get_y(int index)
 {
-    return h % index;
+    return index % h;
 }
 
 int Board::size()
@@ -107,11 +107,6 @@ int Board::get_liberties(int x, int y)
 int *Board::get_lib(int index)
 {
     return &liberties[index];
-}
-
-void Board::kill_check()
-{
-
 }
 
 void Board::dfs(const int index)
